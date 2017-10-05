9296d798014ae980fb9118992be9b9a170eeb62e
Benjamin White <32458896+BenWhite89@users.noreply.github.com>
2017-10-05 09:06:37 -0500

    Clean up code

--- fs-fall17-lab04/main.js
+++ fs-fall17-lab04/main.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var button = document.createElement('button'),
-      buttonText = document.createTextNode('Add Square'),
-      z = 0;
+      buttonText = document.createTextNode('Add Square');
 
   button.appendChild(buttonText);
   document.body.appendChild(button);
@@ -13,9 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var div = document.createElement('div'),
         p = document.createElement('p'),
         count = document.getElementsByClassName('div').length,
-        pText = document.createTextNode(count),
-        z = z + 1;
-
+        pText = document.createTextNode(count);
 
         p.appendChild(pText);
         div.appendChild(p);
@@ -39,27 +36,15 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     div.addEventListener('dblclick', function() {
-      console.log(Number(div.id));
-      
-      
-
       if (div.isSameNode(document.body.lastElementChild) && Number(div.id)%2 === 0) {
         alert('There are no squares to the right!');
       } else if (div.previousElementSibling.id === ''  && Number(div.id)%2 != 0) {
         alert('There are no squares to the left');
       } else if (Number(div.id)%2 === 0) {
-        /*var next = document.getElementById(ids[ids.indexOf(Number(div.id)) + 1]);
-        next.remove();*/
         div.nextElementSibling.remove();
       } else if (Number(div.id)%2 != 0) {
-        /*var prior = document.getElementById(ids[ids.indexOf(Number(div.id)) - 1]);
-        prior.remove();*/
         div.previousElementSibling.remove();
       } else {};
     })
-
   })
-
-  
-
 })
